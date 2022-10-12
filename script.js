7f929c4638065bfdbf69a71b02d43b0c0087696b
Vikas <vikaschoudharyfr@gmail.com>
2022-10-12 15:39:13 +0530

    Move javascript to script.js file

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-// console.log('Hello');
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    let userAnswer = prompt('What sized gird would you like? Type a number.');
+    userAnswer = Number(userAnswer);
+    if (typeof(userAnswer) !== 'number'){
+        alert('Please type a valid number')
+        return;
+    } else if (userAnswer > 100){
+        alert('Number too large, please enter a smaller number')
+    }
+    removeGrid();
+    createGrid(userAnswer);
+})
+
+
+function createGrid(numberOfDiv) {
+
+    for (let i = 0; i < numberOfDiv; i++){
+    const newRow = document.createElement(`div`);
+    newRow.className = "row";
+    document.querySelector('#container').appendChild(newRow);
+    }
+    
+    const DivList = document.querySelectorAll('.row')
+    DivList.forEach((div) => {
+        for (let i=0; i < numberOfDiv; i++){
+        const newDiv = document.createElement('div');
+        newDiv.className = 'box';
+        div.appendChild(newDiv);
+        }
+    }) 
+
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.addEventListener('mouseover', () => {
+            box.classList.add('hover');
+            setTimeout(() => {box.classList.remove('hover')}, 500);
+        })
+    })
+}
+
+function removeGrid() {
+    const rows = document.querySelectorAll('.row').forEach((item) => item.remove());
+}
