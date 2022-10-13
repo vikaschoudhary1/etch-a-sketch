b1015cdbab9cd27a5791b58a4e830edc0dcdd68f
Vikas <vikaschoudharyfr@gmail.com>
2022-10-13 11:14:36 +0530

    Add brightness decrement logic, default grid button

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ button.addEventListener('click', () => {
         return;
     } else if (userAnswer > 100){
         alert('Number too large, please enter a smaller number')
+    } else if(userAnswer <= 100){
+        removeGrid();
+        createGrid(userAnswer);
     }
-    removeGrid();
-    createGrid(userAnswer);
 })
-
 
 function createGrid(numberOfDiv) {
 
@@ -35,12 +35,26 @@ function createGrid(numberOfDiv) {
     const boxes = document.querySelectorAll('.box');
     boxes.forEach((box) => {
         box.addEventListener('mouseover', () => {
+
             const redValue = +randomColorValue();
             const greenValue = +randomColorValue();
             const blueValue = +randomColorValue();
             box.style.background = `rgb(${redValue},${greenValue},${blueValue})`;
+
+            let divBrightness = window.getComputedStyle(box).getPropertyValue('filter');
+            let divBrightnessValue = +divBrightness.slice(11,14);
+
+            if (divBrightness==='none'){
+                box.style.filter = `brightness(1)`;
+            }else if(divBrightness === 'brightness(1)'){        // decimal shifts the number so separate case for 1
+                box.style.filter = `brightness(0.9)`;       
+            } else if ((0 < divBrightnessValue*10 <= 9) ){
+                let decrementBrightness = divBrightnessValue * 10 ;
+                decrementBrightness--;
+                box.style.filter = `brightness(${decrementBrightness/10})`;
+            }
             // box.classList.add('.hover');
-            // setTimeout(() => {box.classList.remove('hover')}, 500);
+            // setTimeout(() => {box.style.background = null}, 500);
         })
     })
 }
@@ -49,6 +63,7 @@ const resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', () => {
     document.querySelectorAll('.box').forEach((box) => {
         box.style.background = null;
+        box.style.filter = 'brightness(1)'
     });
 })
 
@@ -59,6 +74,12 @@ function randomColorValue() {
 function removeGrid() {
     const rows = document.querySelectorAll('.row').forEach((item) => item.remove());
 }
+
+document.querySelector('#trail').addEventListener('click', () => alert('Button out of service'));
+
+document.querySelector('#select10').addEventListener('click', () => { removeGrid();
+    createGrid(10);
+});
 
 })
 
